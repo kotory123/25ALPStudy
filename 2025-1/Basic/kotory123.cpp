111653c9d18a65f293bc8ac0e882152d20bf51a1
kotory123 <kotory123@naver.com>
2025-03-22 18:14:54 +0900

    Create kotory123.cpp

---
+++ 2025-1/Basic/kotory123.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int n, x;
+    cin >> n;
+
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    cin >> x;
+
+ 
+    sort(arr.begin(), arr.end());
+
+    int left = 0, right = n - 1;
+    int count = 0;
+
+    
+    while (left < right) {
+        int sum = arr[left] + arr[right];
+
+        if (sum == x) {
+            count++;  
+            left++;   
+            right--;  
+        }
+        else if (sum < x) {
+            left++;  
+        }
+        else {
+            right--;  
+        }
+    }
+
+    cout << count << endl;
+    return 0;
+}
